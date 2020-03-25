2bd2a6e006d53bc63cac4950c0cd8dbc61eb6a8b
gwalchmay <l.luck@laposte.net>
2020-03-25 16:00:29 +0100

    atelier réalisé

---
+++ wilders.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk');
+const log = console.log;
+
+log(chalk.blue('Jean-Louis Poussin'));
+log(chalk.red('Pierre Brière'));
+log(chalk.yellow('Anaël Dubois'));
+log(chalk.green('William Lecomte'));
